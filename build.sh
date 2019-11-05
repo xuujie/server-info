e54ac95ac8b90f6a31ae7f0e7fbc13137cbeac7e
xujie <xuujie@gmail.com>
2019-11-05 11:06:14 +0800

    test scripts.

---
+++ build.sh
@@ -0,0 +1,3 @@
+gradle clean build
+docker build -t xuujie/system-info:latest .
+docker push xuujie/system-info:latest
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 while :
   do
-    curl http://localhost/server
+    curl http://localhost:8080/server
     let "i = $(($RANDOM%10))"
     echo " sleep $i seconds..."
     sleep $i
